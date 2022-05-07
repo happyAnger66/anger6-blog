84f1c6eb9f58df553bd60234ba000558b85488d6
zhangxiaoan <zhangxiaoan@globaldidi.com>
2022-05-07 09:56:50 +0800

    feat: add share

--- themes/my_landscape/source/js/script.js
+++ themes/my_landscape/source/js/script.js
@@ -75,6 +75,17 @@
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
+    let description = $('meta[name="description"]').attr('content') || '';
+    // QQ空间文字太长会出现 undefined
+    description = description.length > 110 ? (description.substring(0, 110) + '...') : description;
+    // 参考：https://github.com/overtrue/share.js
+    socialShare('#social-share', {
+      description         : description, // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
+      image               : $('#main img').eq(0).attr('src'), // 图片, 默认取网页中第一个img标签
+      sites               : ['weibo', 'qq', 'wechat', 'douban', 'qzone'], // 启用的站点
+      wechatQrcodeTitle   : '微信扫一扫：分享', // 微信二维码提示文字
+      wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>'
+    });
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
